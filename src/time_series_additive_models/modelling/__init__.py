c3c76a5aaa6a29f52830274332885507a62a5a46
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-22 22:22:18 +0100

    feat: add robust OLS covariance estimators

--- src/time_series_additive_models/modelling/__init__.py
+++ src/time_series_additive_models/modelling/__init__.py
@@ -8,16 +8,26 @@ from .basis import (
     piecewise_linear_trend_basis,
     polynomial_trend_basis,
 )
+from .covariance import (
+    CovarianceResult,
+    HCType,
+    heteroskedasticity_consistent_covariance,
+    newey_west_covariance,
+)
 from .estimation import OLSResult, RankDeficientDesignError, fit_ols
 
 __all__ = [
     "BasisMatrix",
+    "CovarianceResult",
+    "HCType",
     "OLSResult",
     "RankDeficientDesignError",
     "TimeTransform",
     "combine_basis_matrices",
     "fit_ols",
     "fourier_seasonal_basis",
+    "heteroskedasticity_consistent_covariance",
+    "newey_west_covariance",
     "piecewise_linear_trend_basis",
     "polynomial_trend_basis",
 ]
